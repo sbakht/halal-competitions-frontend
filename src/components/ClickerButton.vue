39b7c56e110f8eb66ffa48f7c7ab4578602e1237
Saad Bakht <rotk13@gmail.com>
2021-01-13 00:01:03 -0500

    init commit

---
+++ src/components/ClickerButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button>
+    {{ clicker.title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "UserCompetition",
+  props: {
+    clicker: Object,
+  },
+};
+</script>
+
+<style scoped>
+button {
+  margin: 12px;
+  padding: 12px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/UserCompetition.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="title">{{ competition.title }}</div>
+    <div v-for="counter in competition.counters" :key="counter.id">
+      <clicker-button :clicker="counter" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ClickerButton from "./ClickerButton.vue";
+export default {
+  components: { ClickerButton },
+  name: "UserCompetition",
+  props: {
+    competition: Object,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin-bottom: 25px;
+}
+
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  margin: 12px;
+}
+</style>
