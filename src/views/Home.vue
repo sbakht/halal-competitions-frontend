5cd368a25da8c593c7aa77279a630c6e92a3c4d1
Saad Bakht <rotk13@gmail.com>
2021-02-03 19:22:36 -0500

    finish homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
               <router-link to="/register" tag="button">
                 <button
                   type="button"
-                  class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-lg text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Enjoin the Good
                 </button>
@@ -25,77 +25,182 @@
       </div>
     </div>
 
-    <!-- <div class="bg-gray-50 h-screen">
-      <div class="max-w-7xl mx-auto px-6">
-        <div class="flex font-bold pt-12">
+    <div class="bg-gray-50">
+      <div class="max-w-7xl mx-auto px-6 py-12">
+        <div
+          class="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px"
+        >
           <div
-            class="border-b-2 hover:border-indigo-600 cursor-pointer uppercase tracking-wide"
+            class="rounded-tl-lg rounded-tr-lg sm:rounded-tr-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
           >
-            Design Tips
+            <div>
+              <span
+                class="rounded-lg inline-flex p-3 bg-teal-50 text-teal-700 ring-4 ring-white"
+              >
+                <!-- Heroicon name: outline/clock -->
+                <svg
+                  class="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  ></path>
+                </svg>
+              </span>
+            </div>
+            <div class="mt-8">
+              <h3 class="text-lg font-medium">
+                <a href="#" class="focus:outline-none">
+                  <!-- Extend touch target to entire panel -->
+                  <span class="absolute inset-0" aria-hidden="true"></span>
+                  Tasbih Competition
+                </a>
+              </h3>
+              <p class="mt-2 text-sm text-gray-500">
+                We have included 'Subhan Allahi wa bihamdihi' weekly because of
+                the sheer power of that Zikr! (please refer to Sahih al-Bukhari
+                6405)
+              </p>
+            </div>
           </div>
-          <div class="flex-grow border-b-2 pb-4">.</div>
-          <a
-            class="border-b-2 hover:border-indigo-600 cursor-pointer text-indigo-600 font-semibold"
-            href="#"
-            >View All</a
-          >
-        </div>
 
-        <div class="flex flex-wrap justify-between -m-6">
-          <div class="w-full md:w-1/2 lg:w-1/3 px-6 mt-10">
-            <div class="bg-white rounded-lg shadow-md flex-col overflow-hidden">
-              <div class="img img2 h-32 bg-cover"></div>
-              <div class="p-6 flex flex-col justify-between flex-grow">
-                <h3 class="font-bold text-lg">Think Outside The Database</h3>
-                <div class="mt-4">
-                  <a class="text-gray-600 text-sm" href="#">twitter.com</a>
-                </div>
-              </div>
+          <div
+            class="sm:rounded-tr-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
+          >
+            <div>
+              <span
+                class="rounded-lg inline-flex p-3 bg-purple-50 text-purple-700 ring-4 ring-white"
+              >
+                <!-- Heroicon name: outline/badge-check -->
+                <svg
+                  class="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+                  ></path>
+                </svg>
+              </span>
+            </div>
+            <div class="mt-8">
+              <h3 class="text-lg font-medium">
+                <a href="#" class="focus:outline-none">
+                  <!-- Extend touch target to entire panel -->
+                  <span class="absolute inset-0" aria-hidden="true"></span>
+                  Mindful Minutes Competition
+                </a>
+              </h3>
+              <p class="mt-2 text-sm text-gray-500">
+                Compete in reading Quran, listening to Quran, and watching
+                lectures.
+              </p>
             </div>
           </div>
-          <div class="w-full md:w-1/2 lg:w-1/3 px-6 mt-10">
-            <div
-              class="bg-white rounded-lg shadow-md flex flex-col overflow-hidden"
-            >
-              <div class="img img2 h-32 bg-cover"></div>
-              <div class="p-6 flex flex-col justify-between flex-grow">
-                <h3 class="font-bold text-lg">Use Fewer Borders</h3>
-                <div class="mt-4">
-                  <a class="text-gray-600 text-sm" href="#">twitter.com</a>
-                </div>
-              </div>
+
+          <div
+            class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
+          >
+            <div>
+              <span
+                class="rounded-lg inline-flex p-3 bg-light-blue-50 text-light-blue-700 ring-4 ring-white"
+              >
+                <!-- Heroicon name: outline/users -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                </svg>
+              </span>
+            </div>
+            <div class="mt-8">
+              <h3 class="text-lg font-medium">
+                <a href="#" class="focus:outline-none">
+                  <!-- Extend touch target to entire panel -->
+                  <span class="absolute inset-0" aria-hidden="true"></span>
+                  Sadaqah (Charity) Competition
+                </a>
+              </h3>
+              <p class="mt-2 text-sm text-gray-500">
+                Compete in helping out others
+              </p>
             </div>
           </div>
-          <div class="w-full md:w-1/2 lg:w-1/3 px-6 mt-10">
-            <div
-              class="bg-white rounded-lg shadow-md flex flex-col overflow-hidden"
-            >
-              <div class="img img2 h-32 bg-cover"></div>
-              <div class="p-6 flex flex-col justify-between flex-grow">
-                <h3 class="font-bold text-lg">Overlap Images To Add Depth</h3>
-                <div class="mt-4">
-                  <a class="text-gray-600 text-sm" href="#">twitter.com</a>
-                </div>
-              </div>
+
+          <div
+            class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
+          >
+            <div>
+              <span
+                class="rounded-lg inline-flex p-3 bg-yellow-50 text-yellow-700 ring-4 ring-white"
+              >
+                <!-- Heroicon name: outline/cash -->
+                <svg
+                  class="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 10V3L4 14h7v7l9-11h-7z"
+                  ></path>
+                </svg>
+              </span>
+            </div>
+            <div class="mt-8">
+              <h3 class="text-lg font-medium">
+                <a href="#" class="focus:outline-none">
+                  <!-- Extend touch target to entire panel -->
+                  <span class="absolute inset-0" aria-hidden="true"></span>
+                  Fitness Competition
+                </a>
+              </h3>
+              <p class="mt-2 text-sm text-gray-500">
+                A strong believer is better and is more lovable to Allah than a
+                weak believer, and there is good in everyone,--- [Sahih Muslim
+                2664]
+              </p>
             </div>
           </div>
+        </div>
+        <div class="mt-12 text-blue-600 font-bold text-2xl flex justify-center">
+          <a href="https://www.reddit.com/r/halal_competitions"
+            >Visit Our Subreddit</a
+          >
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <style scoped>
 .bg-img {
   background-image: url("https://images.unsplash.com/photo-1590720478291-aaabf8d53c49?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1490&q=80");
-}
-
-.img1 {
-  background-image: url("https://images.unsplash.com/photo-1593975151703-5b13a61f1c1d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80");
-}
-
-.img2 {
-  background-image: url("https://images.unsplash.com/photo-1599577296270-6a1202302973?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80");
 }
 
 @font-face {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          'light-blue': colors.lightBlue,
+          teal: colors.teal,
+          rose: colors.rose,
+        }
+    },
   },
   variants: {
     extend: {},
