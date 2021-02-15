d888e91262a5c07f1f9652d556562d7d5e96b13c
Saad Bakht <rotk13@gmail.com>
2021-02-14 19:25:24 -0500

    deploy

---
+++ precache-manifest.ef0901ed820b818fb8832afc8f7d2b7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f24c8756f8d01d3c0cf6",
+    "url": "/halal-competitions-frontend/css/app.33e0e5ff.css"
+  },
+  {
+    "revision": "f24c8756f8d01d3c0cf6",
+    "url": "/halal-competitions-frontend/js/app.8e55960f.js"
+  },
+  {
+    "revision": "600255010431454d4949",
+    "url": "/halal-competitions-frontend/js/chunk-vendors.7a64adb1.js"
+  },
+  {
+    "revision": "838e6042248e4d2876a2862be058f3b2",
+    "url": "/halal-competitions-frontend/fonts/Uthmani-Hafs.838e6042.woff2"
+  },
+  {
+    "revision": "4aa48165d98e43e7a4cc9190ef8f9c32",
+    "url": "/halal-competitions-frontend/fonts/Uthmani-Hafs.4aa48165.woff"
+  },
+  {
+    "revision": "d71d8ccb7264fb1ca4a4ec9a871849d1",
+    "url": "/halal-competitions-frontend/img/loader.d71d8ccb.gif"
+  },
+  {
+    "revision": "e9c2709fd8a20a92dbd4bdab916cb4cc",
+    "url": "/halal-competitions-frontend/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/halal-competitions-frontend/robots.txt"
+  }
+];
