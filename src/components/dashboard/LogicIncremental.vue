1c283a3ffb803de2108e3f6f15961dcdbb2a08da
Saad Bakht <rotk13@gmail.com>
2021-07-02 01:18:53 -0400

    remove dead code

--- src/components/dashboard/LogicIncremental.vue
+++ src/components/dashboard/LogicIncremental.vue
@@ -5,34 +5,20 @@ export default {
   data() {
     return {
       timeout: null,
-      drag: null,
     };
   },
   methods: {
     onClick(data) {
-      console.log(this.drag);
-      if (!this.drag) {
-        clearTimeout(this.timeout);
-        this.$store.dispatch("increment", data);
-        this.timeout = setTimeout(() => {
-          this.$store.dispatch("save");
-        }, DEBOUNCE_RATE);
-      }
-    },
-    mousedown() {
-      console.log("down");
-      this.drag = true;
-    },
-    mouseup() {
-      console.log("up");
-      this.drag = false;
+      clearTimeout(this.timeout);
+      this.$store.dispatch("increment", data);
+      this.timeout = setTimeout(() => {
+        this.$store.dispatch("save");
+      }, DEBOUNCE_RATE);
     },
   },
   render() {
     return this.$scopedSlots.default({
       incrementFn: this.onClick,
-      mousedown: this.mousedown,
-      mouseup: this.mouseup,
     });
   },
 };
