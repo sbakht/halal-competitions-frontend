4727d2d5f85548771e64e3a46e79b81abf082e3e
Saad Bakht <rotk13@gmail.com>
2021-07-08 20:38:56 -0400

    random colors for progress bar

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -3,29 +3,32 @@
     <div class="relative pt-1">
       <div class="flex mb-2 items-center justify-between">
         <div>
-          <span class="text-xl mb-3 font-semibold inline-block ext-emerald-600">
+          <span
+            :class="`text-xl mb-3 font-semibold inline-block text-${color}-600`"
+          >
             {{ label }}
             <span class="text-md font-normal">{{ current }} / {{ total }}</span>
           </span>
         </div>
         <div class="text-right">
-          <span class="text-xs font-semibold inline-block text-blue-600">
+          <span :class="`text-xs font-semibold inline-block text-${color}-600`">
             {{ parseInt(width) + "%" }}
           </span>
         </div>
       </div>
-      <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+      <div
+        :class="`overflow-hidden h-2 mb-4 text-xs flex rounded bg-${color}-200`"
+      >
         <div
           :style="{ width }"
-          class="
+          :class="`
             shadow-none
             flex flex-col
             text-center
             whitespace-nowrap
             text-white
             justify-center
-            bg-blue-500
-          "
+            bg-${color}-500`"
         ></div>
       </div>
     </div>
@@ -33,6 +36,12 @@
 </template>
 
 <script>
+const colors = ["blue", "red", "green", "indigo", "purple", "yellow", "pink"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 export default {
   props: {
     current: {
@@ -51,6 +60,9 @@ export default {
   computed: {
     width() {
       return (this.current / this.total) * 100 + "%";
+    },
+    color() {
+      return colors[getRandomInt(colors.length)];
     },
   },
 };
