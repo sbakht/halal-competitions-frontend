7a167f90609d10e136ee5bf3945022d5c7f1b3ab
Saad Bakht <rotk13@gmail.com>
2021-07-19 21:11:41 -0400

    fix timeout

--- src/composables/isIncrement.js
+++ src/composables/isIncrement.js
@@ -1,18 +1,17 @@
-import { ref } from "vue";
 import { useStore } from 'vuex'
 
 
 const DEBOUNCE_RATE = 2000;
 
 export default function isIncrement() {
-  const timeout = ref(null);
+  let timeout = null;
 
   const store = useStore()
 
   function increment(data) {
     clearTimeout(timeout);
     store.dispatch("Logger/increment", data);
-    timeout.value = setTimeout(() => {
+    timeout = setTimeout(() => {
       store.dispatch("Logger/save");
     }, DEBOUNCE_RATE);
   }
